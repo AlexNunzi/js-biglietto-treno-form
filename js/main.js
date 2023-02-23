5477b8ef883e7228b3a88e1046ff15698a66ff66
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-23 16:42:39 +0100

    Aggiunto pulsante di reset del form e aggiunto controllo abbozzato per la corretta compilazione dei campi del form.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const startButton = document.querySelector('#startButton');
-const travelDistance = document.querySelector('#travelDistance');
-const userName = document.querySelector('#userName');
-const userAge = document.querySelector('#userAge');
+const resetButton = document.querySelector('#resetButton');
+let travelDistance = document.querySelector('#travelDistance');
+let userName = document.querySelector('#userName');
+let userAge = document.querySelector('#userAge');
 const distancePrice = 0.21;
 const userJuniorDiscount = 20;
 const userSeniorDiscount = 40;
@@ -10,35 +11,47 @@ let ticketType;
 
 startButton.addEventListener('click', function(){
     console.log(parseInt(travelDistance.value));
-    console.log(userAge.value)
-    console.log('pulsante premuto')
+    console.log(userAge.value);
+    console.log('pulsante premuto');
 
     userTravelPrice = parseInt(travelDistance.value) * distancePrice;
     console.log(`prezzo base: ${userTravelPrice}`);
+    console.log(userName.value);
+    console.log(parseInt(travelDistance.value))
+    
+    if(!userName.value || !travelDistance.value || isNaN(parseInt(travelDistance.value)) || userAge.value == 0){
+        console.log('Non sono stati compilati tutti i campi del form oppure alcuni dei dati inseriti potrebbero non essere considerati validi');
+    } else {
+        switch(userAge.value){
+            case '1':
+                console.log('maggiorenne');
+                ticketType = 'Biglietto Standard';
+                break;
+            case '2':
+                console.log('minorenne');
+                userTravelPrice -= ((userTravelPrice * userJuniorDiscount) / 100);
+                ticketType = 'Biglietto Junior';
+                break;
+            case '3':
+                console.log('senior');
+                userTravelPrice -= ((userTravelPrice * userSeniorDiscount) / 100);
+                ticketType = 'Biglietto Senior';
+                break;
+        }
 
+        const trainCarriage = Math.floor((Math.random() * 10)) + 1; // numeri casuali da 1 a 10
+        const cpCode = Math.floor((Math.random() * 10000)) + 90000; // numeri casuali da 90000 a 99999
 
-    switch(userAge.value){
-        case '1':
-            console.log('maggiorenne');
-            ticketType = 'Biglietto Standard';
-            break;
-        case '2':
-            console.log('minorenne');
-            userTravelPrice -= ((userTravelPrice * userJuniorDiscount) / 100);
-            ticketType = 'Biglietto Junior';
-            break;
-        case '3':
-            console.log('senior');
-            userTravelPrice -= ((userTravelPrice * userSeniorDiscount) / 100);
-            ticketType = 'Biglietto Senior';
-            break;
+        console.log(`Il prezzo del biglietto è: ${userTravelPrice.toFixed(2)}€`);
+        console.log(`La tariffa applicata è quella relativa a: ${ticketType}`);
+        console.log(`La carrozza assegnata è la numero: ${trainCarriage}`);
+        console.log(`Il codice CP assegnato è: ${cpCode}`);
     }
-
-    const trainCarriage = Math.floor((Math.random() * 10)) + 1; // numeri casuali da 1 a 10
-    const cpCode = Math.floor((Math.random() * 10000)) + 90000; // numeri casuali da 90000 a 99999
-
-    console.log(`Il prezzo del biglietto è: ${userTravelPrice.toFixed(2)}€`);
-    console.log(`La tariffa applicata è quella relativa a: ${ticketType}`);
-    console.log(`La carrozza assegnata è la numero: ${trainCarriage}`);
-    console.log(`Il codice CP assegnato è: ${cpCode}`)
 });
+
+resetButton.addEventListener('click', function(){
+    console.log('reset');
+    userName.value = '';
+    userAge.value = 0;
+    travelDistance.value = '';
+})
